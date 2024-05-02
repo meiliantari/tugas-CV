2f47a414d6356639148b9f71ba8e1575624d14ee
meiliantari <meiliantariaputri@gmail.com>
2024-05-02 21:52:38 +0800

    first commit

---
+++ tugas.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculum Vitae</title>
+    <style>
+            body {
+        font-family: Arial, sans-serif;
+        line-height: 1.6;
+        margin: 0;
+        padding: 0;
+        background-color: #f4f4f4;
+        }
+
+        .container {
+        max-width: 800px;
+        margin: auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        header {
+        background-color: #00bfff;
+        padding: 20px;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        }
+        
+        .profile img {
+        width: 300px; 
+        height: 300px; 
+        border-radius: 50%; 
+        }
+
+        .profile {
+        text-align: center;
+        color: #fff;
+        }
+        .img {
+            weight: 1%
+        }
+
+        .profile h1 {
+        margin-bottom: 5px;
+        }
+
+        section {
+        padding: 20px;
+        border-bottom: 1px solid #ddd;
+        }
+
+        section:last-child {
+        border-bottom: none;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        h3 {
+            margin-top: 10px;
+            margin-bottom: 5px;
+        }
+
+        p {
+            margin-bottom: 10px;
+        }
+        .foto1 {
+            weight :10px
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="profile">
+                <h1>Meiliantari Mutiara</h1>
+                <p>Email: meiliantariaputri@gmail.com</p>
+                <p>Phone: 08990547370</p>
+                <img src="melly.png" alt="foto profile" clsss="foto1">
+            </div>
+        </header>
+        
+        <section>
+            <h2>Pendidikan</h2>
+            <p>SMK PGRI 1 DENPASAR</p>
+            <p>Institut Bisnis dan Teknologi Indonesia </p>
+        </section>
+        
+        <section>
+            <h2>Experience</h2>
+            <h3>Admin</h3>
+            <p>Bangunan ekspres 2023-2024</p>
+            <p>menjadi Admin e-commerce biasanya berjualan di shopee dan tokopedia</p>
+        </section>
+        
+        <section>
+            <h2>Skills</h2>
+            <p>berbahasa inggris & korea</p>
+            <p>bekerjasama dalam team</p>
+            <p>editing video dan foto</p>
+        </section>
+        
+        <section>
+            <h2>Projects</h2>
+            <h3>project videography</h3>
+            <p>Description: membuat konten untuk perusahaan</p>
+            <h3>Project fotography</h3>
+            <p>Description: memfoto produck yg di sediakan untuk penjualan online </p>
+        </section>
+    </div>
+</body>
+</html>
